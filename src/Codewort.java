973ff6b91f7f5989bce9eaaecae9896ca167c22a
LynnH123 <husseini_line2009@hotmail.com>
2024-10-27 19:42:02 +0100

    Create Codewort.java

---
+++ src/Codewort.java
@@ -0,0 +1,11 @@
+public class Codewort {
+    public static void main(String[] args) {
+
+        String feedbackURL = "https://ilias.hhu.de/ilias.php?ref_id=1781771&tableview_id=991&dcl_rl_1033_991_table_nav=rank:asc:0&cmd=listRecords&cmdClass=ildclrecordlistgui&cmdNode=y8:n9:ai&baseClass=ilrepositorygui";
+
+        String githubUsername = "LynnH123";
+
+        System.out.println("Feedback-URL: " + feedbackURL);
+        System.out.println("GitHub-Benutzername: " + githubUsername);
+    }
+}
